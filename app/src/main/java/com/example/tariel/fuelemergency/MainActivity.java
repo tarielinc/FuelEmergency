9e2885bd826154c460c0342b4e8b6c54cfc9a5e3
Tariel Hambardzumyan <tariel.hambardzumyan@gmail.com>
2017-09-26 22:14:25 +0400

    Action Bar and Permission bug

--- app/src/main/java/com/example/tariel/fuelemergency/MainActivity.java
+++ app/src/main/java/com/example/tariel/fuelemergency/MainActivity.java
@@ -4,7 +4,10 @@ package com.example.tariel.fuelemergency;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,7 +17,7 @@ import com.google.android.gms.maps.model.LatLng;
 public class MainActivity extends FragmentActivity implements LocationListener {
 
 
-    private GoogleMap map;
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +26,16 @@ public class MainActivity extends FragmentActivity implements LocationListener {
 
     }
 
+    private void requestPermission() {
+
+
+    }
+
     @Override
     public void onLocationChanged(Location location) {
         LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
         CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 10);
-        map.animateCamera(cameraUpdate);
+        googleMap.animateCamera(cameraUpdate);
     }
 
     @Override
