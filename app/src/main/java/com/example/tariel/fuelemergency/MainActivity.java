3d828e84d2655acd781e40ea4005597dbcafdb20
Tariel Hambardzumyan <tariel.hambardzumyan@gmail.com>
2017-11-02 17:21:51 +0400

    some bugs

--- app/src/main/java/com/example/tariel/fuelemergency/MainActivity.java
+++ app/src/main/java/com/example/tariel/fuelemergency/MainActivity.java
@@ -1,37 +1,32 @@
 package com.example.tariel.fuelemergency;
 
 
-        import com.google.android.gms.maps.CameraUpdate;
-        import com.google.android.gms.maps.CameraUpdateFactory;
         import com.google.android.gms.maps.GoogleMap;
         import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
         import com.google.android.gms.maps.OnMapReadyCallback;
         import com.google.android.gms.maps.SupportMapFragment;
         import com.google.android.gms.maps.model.BitmapDescriptorFactory;
         import com.google.android.gms.maps.model.LatLng;
+        import com.google.android.gms.maps.model.Marker;
         import com.google.android.gms.maps.model.MarkerOptions;
-
-        import android.Manifest;
-        import android.content.pm.PackageManager;
-        import android.location.Location;
         import android.os.Bundle;
+        import android.content.pm.PackageManager;
         import android.support.annotation.NonNull;
         import android.support.v4.app.ActivityCompat;
         import android.support.v4.content.ContextCompat;
         import android.support.v7.app.AppCompatActivity;
-        import android.widget.Toast;
-
-        import org.json.JSONObject;
+        import com.google.firebase.analytics.FirebaseAnalytics;
 
 
 public class MainActivity extends AppCompatActivity implements
         OnMyLocationButtonClickListener,
         OnMapReadyCallback,
+        GoogleMap.OnMarkerClickListener,
         ActivityCompat.OnRequestPermissionsResultCallback {
 
     private static final LatLng GAS1 = new LatLng(40.193076, 44.533152);
 
-
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
 
@@ -39,9 +34,9 @@ public class MainActivity extends AppCompatActivity implements
     private boolean mPermissionDenied = false;
 
     private GoogleMap mMap;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -68,11 +63,11 @@ public class MainActivity extends AppCompatActivity implements
 
 
     private void enableMyLocation() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
             // Permission to access the location is missing.
                 PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
-                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+                        android.Manifest.permission.ACCESS_FINE_LOCATION, true);
         } else if (mMap != null) {
             // Access to the location has been granted to the app.
             mMap.setMyLocationEnabled(true);
@@ -92,7 +87,7 @@ public class MainActivity extends AppCompatActivity implements
         }
 
         if (PermissionUtils.isPermissionGranted(permissions, grantResults,
-                Manifest.permission.ACCESS_FINE_LOCATION)) {
+                android.Manifest.permission.ACCESS_FINE_LOCATION)) {
             // Enable the my location layer if the permission has been granted.
             enableMyLocation();
         } else {
@@ -118,6 +113,22 @@ public class MainActivity extends AppCompatActivity implements
         PermissionUtils.PermissionDeniedDialog
                 .newInstance(true).show(getSupportFragmentManager(), "dialog");
     }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+
+        String name= marker.getTitle();
+
+        String markerName;
+        switch (name.equalsIgnoreCase(markerName)){
+            case "PAYT":
+
+        }
+
+
+        return false;
+    }
+
 
 }
 
--- app/src/main/java/com/example/tariel/fuelemergency/PermissionUtils.java
+++ app/src/main/java/com/example/tariel/fuelemergency/PermissionUtils.java
@@ -13,10 +13,7 @@ import android.widget.Toast;
 
 public abstract class PermissionUtils {
 
-    /**
-     * Requests the fine location permission. If a rationale with an additional explanation should
-     * be shown to the user, displays a dialog that triggers the request.
-     */
+
     public static void requestPermission(AppCompatActivity activity, int requestId,
                                          String permission, boolean finishActivity) {
         if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
@@ -30,12 +27,7 @@ public abstract class PermissionUtils {
         }
     }
 
-    /**
-     * Checks if the result contains a {@link PackageManager#PERMISSION_GRANTED} result for a
-     * permission from a runtime permissions request.
-     *
-     * @see android.support.v4.app.ActivityCompat.OnRequestPermissionsResultCallback
-     */
+
     public static boolean isPermissionGranted(String[] grantPermissions, int[] grantResults,
                                               String permission) {
         for (int i = 0; i < grantPermissions.length; i++) {
@@ -46,19 +38,14 @@ public abstract class PermissionUtils {
         return false;
     }
 
-    /**
-     * A dialog that displays a permission denied message.
-     */
+
     public static class PermissionDeniedDialog extends DialogFragment {
 
         private static final String ARGUMENT_FINISH_ACTIVITY = "finish";
 
         private boolean mFinishActivity = false;
 
-        /**
-         * Creates a new instance of this dialog and optionally finishes the calling Activity
-         * when the 'Ok' button is clicked.
-         */
+
         public static PermissionDeniedDialog newInstance(boolean finishActivity) {
             Bundle arguments = new Bundle();
             arguments.putBoolean(ARGUMENT_FINISH_ACTIVITY, finishActivity);
@@ -89,14 +76,7 @@ public abstract class PermissionUtils {
         }
     }
 
-    /**
-     * A dialog that explains the use of the location permission and requests the necessary
-     * permission.
-     * <p>
-     * The activity should implement
-     * {@link android.support.v4.app.ActivityCompat.OnRequestPermissionsResultCallback}
-     * to handle permit or denial of this permission request.
-     */
+
     public static class RationaleDialog extends DialogFragment {
 
         private static final String ARGUMENT_PERMISSION_REQUEST_CODE = "requestCode";
@@ -105,18 +85,6 @@ public abstract class PermissionUtils {
 
         private boolean mFinishActivity = false;
 
-        /**
-         * Creates a new instance of a dialog displaying the rationale for the use of the location
-         * permission.
-         * <p>
-         * The permission is requested after clicking 'ok'.
-         *
-         * @param requestCode    Id of the request that is used to request the permission. It is
-         *                       returned to the
-         *                       {@link android.support.v4.app.ActivityCompat.OnRequestPermissionsResultCallback}.
-         * @param finishActivity Whether the calling Activity should be finished if the dialog is
-         *                       cancelled.
-         */
         public static RationaleDialog newInstance(int requestCode, boolean finishActivity) {
             Bundle arguments = new Bundle();
             arguments.putInt(ARGUMENT_PERMISSION_REQUEST_CODE, requestCode);
